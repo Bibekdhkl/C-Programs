13be3dda96b20f670c1e7a8c89d32fa8519bcea0
Shobha-27 <88886122+Shobha-27@users.noreply.github.com>
2021-10-30 11:57:46 +0530

    Update Factorial of a number.c

--- Factorial of a number.c
+++ Factorial of a number.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
-
-unsigned long long int factorial (unsigned long long int n) {
-
-    if (n == 0)
-
-        return 1;
-
-    else
-
-        return n * factorial (n - 1);
-
-}
-
-int main() 
-
+unsigned long long int factorial (unsigned long long int n) 
 {
-
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial (n - 1);
+}
+int main() 
+{
     unsigned long long int n;
-
     scanf ("%llu", &n);
-
     printf ("%llu", factorial(n));
-
     return 0;
-
 }
