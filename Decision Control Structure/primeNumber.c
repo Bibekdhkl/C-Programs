0914203877024351d5919461a81233f9b112cc64
imranjansharma <91980705+imranjansharma@users.noreply.github.com>
2021-10-30 22:38:15 +0530

    Update primeNumber.c

--- Decision Control Structure/primeNumber.c
+++ Decision Control Structure/primeNumber.c
@@ -1,10 +1,12 @@
-//Program to print all the prime number from  1 t0 10
+//Program to print all the prime number from  1 t0 n (n entered by user)
 #include <stdio.h>
 
 int main()
 {
-      int i, j;
-      for (i = 1; i <= 10; i++)
+      int i, j, n;
+      printf("Enter the number : ");
+      scanf("%d",&n);
+      for (i = 1; i <= n; i++)
       {
             for (j = 2; j < i; j++)
             {
